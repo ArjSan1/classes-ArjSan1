1368ad13e5dbf6134ff7d6819262376a2225eeec
ArjSan1 <66225751+ArjSan1@users.noreply.github.com>
2020-10-26 11:57:51 -0500

    Finished primitive types overflow and rounding notes.

---
+++ PrimitveTypes.java
@@ -0,0 +1,35 @@
+
+/**
+ * Write a description of class PrimitveTypes here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class PrimitveTypes
+{
+    /*
+     *  Every value is either, one of 8 primitives, or a reference to an object
+     *  Java is strongly typed-every variable is of a specific type.
+     *  
+     *  Primitive Data Types
+     *  boolean
+     *  char
+     *  byte
+     *  short
+     *  long
+     *  int
+     *  float
+     *  double
+     *  
+     *  
+     *  Overflow:
+     *  results of opeartion are truncated to fit in the size of the type
+     *  int can store -2147483648 to 2147483648
+     *  if overflow is a possibility use long
+     *  Rounding errors
+     *  -floating point numbers have limited precision
+     *  round off errors may occur
+     *  a double has more precision than a float
+     *  care is needed when comparing floating-point values (i.e., use of epsilon values)
+     */
+}
