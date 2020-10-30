7a87298552e1a418d3250ac76e54619397da2777
ArjSan1 <66225751+ArjSan1@users.noreply.github.com>
2020-10-30 09:50:23 -0500

    Finished  Operators, Integer Division, and Mod video notes

---
+++ Operators.java
@@ -0,0 +1,16 @@
+
+/**
+ * Write a description of class Operators here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Operators
+{
+/* perform operations on 1,2, or 3 operands
+ * returns a result
+ * have a precedence order (order in which operators are evaluated)
+ * have associativity (order in which operators with the same precedence are evaluated)
+ * 
+ */
+}
